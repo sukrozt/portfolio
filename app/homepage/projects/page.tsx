60588fec75aef08a77dd4914c476793777e921c8
Şükriye Öztürk <b2210356110.cs@hacettepe.edu.tr>
2025-02-17 00:51:08 +0300

    map structures added

--- app/homepage/projects/page.tsx
+++ app/homepage/projects/page.tsx
@@ -1,67 +1,66 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const projects = [
+  {
+    id: 1,
+    name: "Smart Home System",
+    image: "/projects/smart home.png",
+    link: "https://github.com/sukrozt/bbm104/tree/main/assignments/as2",
+    description:
+      "A Smart Home System which has adjustable components and with an energy consume calculator. Used all OOP elements.",
+    language: "Java",
+  },
+  {
+    id: 2,
+    name: "GRC Software",
+    image: "/projects/GRC cyber.png",
+    link: "https://github.com/sukrozt/GRC-Software",
+    description:
+      "A GRC Software which applies all cyber security protocols suitable with the companies. Still in development with NoSQL DBMS, Python.",
+    language: "Python",
+  },
+  {
+    id: 3,
+    name: "Pizza Delivery System",
+    image: "/projects/pizza.png",
+    link: "https://github.com/sukrozt/Global-AI-Hub-Bootcamp",
+    description:
+      "A mini pizza delivery system that has ingredients of a pizza. There is an UI for the purchases.",
+    language: "Python",
+  },
+  {
+    id: 4,
+    name: "Flappy Game",
+    image: "/projects/bird.png",
+    link: "https://github.com/sukrozt/flappy",
+    description:
+      "A mini 2D Unity game which is the endless fly of characters in a map with columns.",
+    language: "C#, Platform: Unity",
+  },
+];
+
 export default function Page() {
   return (
     <div className="container mx-auto flex flex-row items-start justify-start gap-10 text-orange-950 text-xl mt-10">
-      {/* Smart Home System */}
-      <div className="flex flex-col items-center">
-        <Link href="https://github.com/sukrozt/bbm104/tree/main/assignments/as2" target="_blank">
-          <Image
-            src="/projects/smart home.png"
-            alt="Smart Home System"
-            width={150}
-            height={50}
-            className="opacity-100 transition-opacity hover:opacity-50"
-          />
-        </Link>
-        <p className="mt-2 text-center text-lg">A Smart Home System which has adjustable components and with a energy consume calculator. Used all OOP elements.</p>
-        <p className="mt-2 text-center text-lg">Language: Java</p>
-      </div>
-
-      {/* GRC Software */}
-      <div className="flex flex-col items-center">
-        <Link href="https://github.com/sukrozt/GRC-Software" target="_blank">
-          <Image
-            src="/projects/GRC cyber.png"
-            alt="GRC Software"
-            width={150}
-            height={50}
-            className="opacity-100 transition-opacity hover:opacity-50"
-          />
-        </Link>
-        <p className="mt-2 text-center text-lg">A GRC Software which applies all cyber security protocols suitable with the companies. Been developing still with NoSQL DBMS, Python language.</p>
-      </div>
-
-      {/* Pizza Delivery System */}
-      <div className="flex flex-col items-center">
-        <Link href="https://github.com/sukrozt/Global-AI-Hub-Bootcamp" target="_blank">
-          <Image
-            src="/projects/pizza.png"
-            alt="Pizza Delivery System"
-            width={250}
-            height={50}
-            className="opacity-100 transition-opacity hover:opacity-50"
-          />
-        </Link>
-        <p className="mt-2 text-center text-lg">A mini pizza delivery system that has ingredients of a pizza. There is an UI for the purchases.</p>
-        <p className="mt-2 text-center text-lg">Language: Python</p>
-      </div>
-
-      {/* Flappy Game */}
-      <div className="flex flex-col items-center">
-        <Link href="https://github.com/sukrozt/flappy" target="_blank">
-          <Image
-            src="/projects/bird.png"
-            alt="Flappy Game"
-            width={200}
-            height={200}
-            className="opacity-100 transition-opacity hover:opacity-50"
-          />
-        </Link>
-        <p className="mt-2 text-center text-lg">A mini 2D Unity game which is the endless fly of characters in a map with columns.</p>
-        <p className="mt-2 text-center text-lg">Language: C#, Platform: Unity</p>
-      </div>
+      {projects.map((project) => (
+        <div key = {project.id} className="flex flex-col items-center w-60">
+          <Link href={project.link} target="_blank">
+            <Image
+              src={project.image}
+              alt={project.name}
+              width={200}
+              height={180}
+              className="opacity-100 transition-opacity hover:opacity-50"
+            />
+          </Link>
+          <p className="mt-2 text-center text-lg">{project.description}</p>
+          {project.language &&
+          (<p className="mt-2 text-center text-lg">
+            Language: {project.language}</p>
+            )}
+        </div>
+      ))}
     </div>
   );
 }
--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,47 +1,47 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const contacts = [
+  {
+    id: 1,
+    name: "LinkedIn",
+    image: "/linkedin.png",
+    link: "https://www.linkedin.com/in/%C5%9F%C3%BCkriye-%C3%B6zt%C3%BCrk-8a0394225/",
+    alt: "LinkedIn Account",
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    image: "/github.png",
+    link: "https://github.com/sukrozt",
+    alt: "GitHub Account",
+  },
+  {
+    id: 3,
+    name: "E-Mail",
+    image: "/mail.png",
+    link: "mailto:sukriyeo13@gmail.com",
+    alt: "E-Mail Address",
+  },
+];
+
+
 export default function Contact() {
   return (
-    <div className="flex flex-col items-end justify-start gap-6 text-orange-950 text-xl mt-10">
-      {/* LinkedIn */}
-      <div className="flex items-center gap-2n">
-        <Link href="https://www.linkedin.com/in/%C5%9F%C3%BCkriye-%C3%B6zt%C3%BCrk-8a0394225/" target="_blank">
-          <Image
-            src="/linkedin.png"
-            alt="LinkedIn Account"
-            width={50}
-            height={50}
-            className="opacity-100 transition-opacity hover:opacity-50"
-            />
+    <div className="flex flex-col items-end justify-start gap-6 text-orange-950 text-xl mt-10 sm:flex-row sm:flex-wrap sm:justify-center">
+      {contacts.map((contact) => (
+      <div key={contact.id} className="flex flex-col items-center w-60">
+        <Link href={contact.link} target="_blank">
+        <Image
+          src={contact.image}
+          alt={contact.name}
+          width={50}
+          height={50}
+          className="opacity-100 transition-opacity hover:opacity-50"
+        />  
         </Link>
       </div>
-
-      {/* GitHub */}
-      <div className="flex items-center gap-2">
-        <Link href="https://github.com/sukrozt" target="_blank">
-          <Image
-            src="/github.png"
-            alt="GitHub Account"
-            width={50}
-            height={50}
-            className="opacity-100 transition-opacity hover:opacity-50"
-            />
-        </Link>
-      </div>
-
-      {/* E-Mail */}
-      <div className="flex items-center gap-2">
-        <Link href="mailto:sukriyeo13@gmail.com">
-          <Image
-            src="/mail.png"
-            alt="E-Mail Address"
-            width={50}
-            height={50}
-            className="opacity-100 transition-opacity hover:opacity-50"
-            />
-        </Link>
-      </div>
+      ))}
     </div>
   );
 }
